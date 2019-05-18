a30605f36ee2f50981dc4d3262e969df88f6ef21
Takashi Masuyama <mamewotoko@gmail.com>
2019-05-18 12:31:49 +0900

    upload screen capture

--- ci/upload.sh
+++ ci/upload.sh
@@ -3,5 +3,7 @@
 
 COMMIT=$(echo $TRAVIS_COMMIT | sed 's!^\(.\{10\}\).*!\1!')
 
-ARCHIVE=app/build/outputs/apk/release/app-release.apk
-./gdrive upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR --name screenshot-${COMMIT}.apk screenshot.png
+ARCHIVE=output
+./gdrive upload -r --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR \
+         --name output-${COMIT} \
+         output
