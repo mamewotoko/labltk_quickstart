7976af89ed475c56cee68c1a9fe34ebcfc32ca1d
Takashi Masuyama <mamewotoko@gmail.com>
2019-05-18 13:13:43 +0900

    modify test command

--- ci/upload.sh
+++ ci/upload.sh
@@ -5,5 +5,5 @@ COMMIT=$(echo $TRAVIS_COMMIT | sed 's!^\(.\{10\}\).*!\1!')
 
 ARCHIVE=output
 ./gdrive upload -r --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR \
-         --name output-${COMIT} \
-         output
+         --name output-${COMMIT} \
+         $ARCHIVE
