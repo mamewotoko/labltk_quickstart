b5cdbce87ed7b7c6b10d295349117443c487cbd1
Takashi Masuyama <mamewotoko@gmail.com>
2020-01-03 12:48:19 +0900

    modify script to download google drive cli

--- ci/upload.sh
+++ ci/upload.sh
@@ -5,5 +5,5 @@ COMMIT=$(echo $TRAVIS_COMMIT | sed 's!^\(.\{10\}\).*!\1!')
 
 ARCHIVE=output
 ./gdrive upload -r --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR \
-         --description "output-${COMMIT} ${TRAVIS_JOB_WEB_URL}"\
+         --description "output-${COMMIT} ${TRAVIS_JOB_WEB_URL}" \
          $ARCHIVE
